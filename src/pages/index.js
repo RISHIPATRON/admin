910fafabcc864ffbbb86ddf12a34c4e5ae2562cc
Harman <heart4harman@gmail.com>
2022-02-27 20:06:43 +0530

    Clone Abhi Code and New Setup

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+
+import styles from '../../styles/Home.module.css'
+
+export default function Home() {
+  return (
+
+  <div className="centerDiv">
+
+    <div style={{fontSize:'30px'}}> Via Time </div>
+    
+    </div>
+
+  )
+}
